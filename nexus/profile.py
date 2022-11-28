38212a1e2e635a3acafbd4e1a4e00c0d5080cb12
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2022-11-28 16:37:19 -0500

    Topic extraction

--- nexus/profile.py
+++ nexus/profile.py
@@ -1,9 +1,17 @@
-from typing import List
+from typing import List, Optional
 import preprocessor as p
-from nomic import AtlasClient
+from nomic import AtlasClient, CohereEmbedder
 from utils import Utils
 import jsonlines
+import json
 from tqdm import tqdm
+from loguru import logger
+import numpy as np
+from sklearn.cluster import KMeans
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Profile:
@@ -21,7 +29,8 @@ class Profile:
         """
         lookup_amount = 10000
         for user in users:
-            tweets = [{"text": p.clean(tweet["full_text"]), "created_at": tweet["created_at"]} for tweet in self.utils.user_lookup(user, lookup_amount)]
+            tweets = [{"text": p.clean(tweet["full_text"]), "created_at": tweet["created_at"]} for tweet in
+                      self.utils.user_lookup(user, lookup_amount)]
             with jsonlines.open(f'{outdir}/{user}_tweets.jsonl', mode='a') as writer:
                 for idx, tweet in enumerate(tweets):
                     if len(tweet["text"]) < 10:
@@ -38,9 +47,18 @@ class Profile:
                             num_workers=10
                             )
 
-    def create_social_profile_sns(self, map_name: str, map_description: str, users: List[str], outdir: str):
+    def create_social_profile_sns(self,
+                                  map_name: str,
+                                  map_description: str,
+                                  users: List[str],
+                                  outdir: Optional[str] = '',
+                                  topics: bool = False,
+                                  embedding_path: str = ''
+    ):
         """
 
+        :param embedding_path: path of npy file for topic extraction
+        :param topics: indicating whether or not you want to automatic topic extraction
         :param map_name: name of atlas map
         :param map_description: description of atlas map
         :param users: handle of twitter user that is used to create the social profile
@@ -48,28 +66,82 @@ class Profile:
         """
         all_tweets = []
 
-        for user in tqdm(users):
-            tweets = self.utils.user_lookup_sns(user, 10000)
-            with jsonlines.open(f'{outdir}/{user}_tweets.jsonl', mode='a') as writer:
-                for idx, tweet in enumerate(tweets):
-                    if len(tweet["full_text"]) < 20 or None:
-                        tweets.pop(idx)
-                        continue
 
-                    tweet["created_at"] = str(tweet["created_at"])
-                    tweet["full_text"] = p.clean(tweet["full_text"])
-                    all_tweets.append(tweet)
-                    writer.write(tweet)
+        for user in tqdm(users):
+            try:
+                logger.info(f"Loading {user}'s tweets from disk")
+                data_path = os.path.join(outdir, f"{user}_tweets.jsonl")
+                with jsonlines.open(data_path, mode="r") as tweets:
+                    for tweet in tweets:
+                        all_tweets.append(tweet)
+            except BaseException:
+                logger.info(f"Not on disk! scraping {users}'s tweets now")
+                tweets = self.utils.user_lookup_sns(user, 10000)
+                with jsonlines.open(f'{outdir}/{user}_tweets.jsonl', mode='a') as writer:
+                    for idx, tweet in enumerate(tweets):
+                        tweet["full_text"] = p.clean(tweet["full_text"])
+                        if len(tweet["full_text"]) > 30:
+                            tweet["created_at"] = str(tweet["created_at"])
+                            all_tweets.append(tweet)
+                            writer.write(tweet)
+
+
+            for idx, tweet in enumerate(all_tweets):
+                tweet["id"] = str(idx)
+
+        if topics:
+            n_cluster_docs = [40]
+            for n_clusters in n_cluster_docs:
+                logger.info(f"computing {n_clusters} cluster layer")
+                try:
+                    with open(f"data/cluster_labels/{users[0]}_id_to_cluster_label_{n_clusters}", "r") as f:
+                        id_to_cluster_label = json.load(f)
+                    logger.info("Loaded all resources from disk")
+                    print(id_to_cluster_label[-1])
+                except BaseException:
+                    # remake clusters
+                    id_to_cluster_label = {}
+
+                    try:
+                        logger.info("Loading embeddings from disk.")
+                        embeddings = np.load(embedding_path)
+                    except BaseException:
+                        logger.info("Embedding with Cohere")
+                        cohere_api_key = os.getenv("COHERE_KEY")
+                        embedder = CohereEmbedder(cohere_api_key=cohere_api_key)
+                        embeddings = np.array(embedder.embed(texts=[datum['full_text'] for datum in all_tweets])).squeeze()
+                        with open(embedding_path, 'wb') as f:
+                            np.save(f, embeddings)
+                    logger.info("Running Kmeans to generate clusters")
+                    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(embeddings)
+                    for datum, cluster_id in zip(all_tweets, [int(i) for i in list(kmeans.labels_)]):
+                        id_to_cluster_label[datum['id']] = cluster_id
+
+                    with open(f'data/cluster_labels/{users[0]}_id_to_cluster_label_{n_clusters}', 'w') as f:
+                        json.dump(id_to_cluster_label, f)
+                print(len(all_tweets))
+                logger.info("Computing Topics")
+                cluster_rarity_list = self.utils.create_topics(all_tweets, id_to_cluster_label=id_to_cluster_label)
+
+                for idx, datum in enumerate(all_tweets):
+                    datum_cluster = id_to_cluster_label[str(idx)]
+                    datum[f"cluster_{n_clusters}"] = datum_cluster
+                    datum[f"topic_{n_clusters}"] = cluster_rarity_list[datum_cluster][1]
 
         self.atlas.map_text(data=all_tweets,
                             indexed_field='full_text',
                             is_public=True,
                             map_name=map_name,
                             map_description=map_description,
-                            colorable_fields=["user"],
+                            colorable_fields=["topic_40","user"],
                             )
 
 
 if __name__ == "__main__":
     profiler = Profile()
-    profiler.create_social_profile_sns(outdir='data/', map_name='Social Profile of the current POTUS', map_description="A social profile of the latest POTUS Joe Biden, with Nomic's text embedder created by Yuvanesh Anand", users=["JoeBiden", "POTUS"])
+    profiler.create_social_profile_sns(outdir='data/',
+                                       map_name='Social Profile of the current POTUS',
+                                       map_description="A social profile of the latest POTUS Joe Biden, with Nomic's text embedder created by Yuvanesh Anand",
+                                       users=["JoeBiden", "POTUS"],
+                                       topics=True,
+                                       embedding_path="embeddings/JoeBiden.npy")
--- nexus/utils.py
+++ nexus/utils.py
@@ -1,11 +1,15 @@
-from typing import List, Dict
-from nomic import AtlasClient
 import tweepy
 from dotenv import load_dotenv
 import os
 import snscrape.modules.twitter as sntwitter
 from tqdm import tqdm
 from loguru import logger
+from collections import defaultdict
+from nltk.corpus import stopwords
+from typing import List, Dict, Tuple
+import string
+import re
+stop_words = set(stopwords.words('english'))
 
 load_dotenv()
 
@@ -20,7 +24,6 @@ class Utils:
         auth.set_access_token(access_token, access_token_secret)
 
         self.api = tweepy.API(auth)
-        self.atlas = AtlasClient
     
     def user_lookup_tweepy(self, user: str, quantity: int):
         """Obtain Tweets from a specific user.(only up to 3200 tweets)
@@ -36,13 +39,6 @@ class Utils:
         ).items(quantity)  # tweepy.Cursor allows for pagination due to the single request tweet limitations
 
         for tweet in tweets:
-            query.append(tweet._json)
-
-        tweet_more = tweepy.Cursor(
-                        self.api.user_timeline, max_id=query[-1], screen_name=user, count=200, tweet_mode="extended"
-                    ).items(quantity)
-
-        for tweet in tweet_more:
             query.append(tweet._json)
 
         return query
@@ -62,6 +58,74 @@ class Utils:
             query.append({"full_text": tweet.content, "tweet_link": f"https://twitter.com/{tweet.user.username}/status/{tweet.id}" , "created_at": tweet.date, "tweet_id": tweet.id, "user": tweet.user.username})
 
         return query
+
+    def create_topics(self, documents: List[Dict], id_to_cluster_label: Dict, id_field='id', text_field='full_text'):
+        """
+            A fast implementation of cluster n-gram frequency inverse corpus n-gram frequency
+            n-grams cannot be a cluster label if:
+            1. They contain stopwords
+            2. They contain digits.
+            """
+        ngram_corpus_frequency = defaultdict(lambda: 0)
+        ngram_cluster_frequency = defaultdict(lambda: defaultdict(lambda: 0))
+        ngram_rarity_cluster_list = []
+
+        documents.pop(-1)
+
+        for datum in documents:
+            cluster_id = id_to_cluster_label[datum[id_field]]
+
+            text = datum[text_field].translate(str.maketrans('', '', string.punctuation))
+            tokens = re.split(r'\W+', text)
+
+            ngrams_in_document = set()
+            for i in range(len(tokens) - 1):
+                unigram = tokens[i]
+                bigram = " ".join(tokens[i:i + 2])
+
+                if tokens[i] in stop_words or tokens[i + 1] in stop_words:
+                    continue
+
+                unigram_contains_digits = any(j.isdigit() for j in tokens[i])
+                bigram_contains_digits = unigram_contains_digits or any(j.isdigit() for j in tokens[i + 1])
+
+                # ngrams only count if they do not contain digits.
+                # If an ngram appears multiple times in a document, count it only once.
+
+                if not unigram_contains_digits and not unigram in ngrams_in_document:
+                    ngram_corpus_frequency[unigram] += 1
+                    ngram_cluster_frequency[cluster_id][unigram] += 1
+                    ngrams_in_document.add(unigram)
+
+                # if bigram.isalpha():
+                if not bigram_contains_digits and not bigram in ngrams_in_document:
+                    ngram_corpus_frequency[bigram] += 1
+                    ngram_cluster_frequency[cluster_id][bigram] += 1
+                    ngrams_in_document.add(bigram)
+            ngrams_in_document = set()
+
+        for cluster_id, ngram_frequencies in ngram_cluster_frequency.items():
+            ngram_rarity: List[Tuple] = []
+            for ngram, cluster_frequency in ngram_frequencies.items():
+                if cluster_frequency > 5:  # ensure they are not just rare tokens in the corpus, but do occur with some freqency in the clusters
+                    rarity = float(cluster_frequency) / float(ngram_corpus_frequency[ngram])
+                    ngram_rarity.append((ngram, rarity))
+            try:
+                rarest_ngrams = sorted(ngram_rarity, key=lambda item: item[1], reverse=True)[
+                                :5]  # [0][0] #first [0] is to get the tuple, second [0] is to access the first element of the tuple
+                rarest_ngram = sorted(rarest_ngrams, key=len, reverse=True)[0][0]
+            except IndexError:
+                print("empty abstract")
+
+            print(cluster_id)
+            print(rarest_ngrams)
+            print()
+
+            ngram_rarity_cluster_list.append((cluster_id, rarest_ngram))
+
+        ngram_rarity_cluster_list.sort(key=lambda item: item[0])
+        print(ngram_rarity_cluster_list)
+        return ngram_rarity_cluster_list  # [0] is cluster, [1] is topic label
 
 
     
